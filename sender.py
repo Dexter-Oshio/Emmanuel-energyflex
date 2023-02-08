b123aea94ed3bf1782f97c0616885a5451f0c56b
Emmanuel Oshione <oshionedj@gmail.com>
2023-02-08 20:45:36 +0300

    first commit

---
+++ sender.py
@@ -0,0 +1,39 @@
+import requests
+import random
+import time
+import paho.mqtt.client as mqtt
+
+# Set Mosquitto (MQTT) parameters
+broker_address = "localhost"
+port = 1883
+topic = "sensor_data"
+
+
+# Create a function to publish simulated sensor data
+def publish_data():
+    # Generate simulated sensor data
+    data = str(random.randint(0, 100))
+    print("Publishing data: ", data)
+
+
+# Create a MQTT client
+client = mqtt.Client()
+
+# Connect to the MQTT broker
+client.connect(broker_address, port)
+
+# Publish sensor data every 5 seconds
+try:
+    while True:
+        publish_data()
+        time.sleep(5)
+
+# Handle any exceptions that may occur
+except KeyboardInterrupt:
+    print("Connection closed")
+
+except Exception as e:
+    print("Error: ", e)
+
+finally:
+    client.disconnect()
